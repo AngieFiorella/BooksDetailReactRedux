9a57b3fcefb4b37913b62e98052c6a5479c409df
Angie Barrantes <abarrantes@rcp.pe>
2018-06-14 14:39:43 -0500

    Added reducers and containers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return 
+    [
+			{ title: 'Harry Potter'},
+			{ title: 'Cat\'s life'},
+			{ title: 'JavaScript for Dummies'},
+			{ title: 'Programming with games'}
+    ]
+} 
